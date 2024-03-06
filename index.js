e00ced21b78d50078a92d79f5b7f734f304023d4
neeraj-git-workspace <neerajlag1998@gmail.com>
2024-03-06 15:58:01 +0000

    new

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true});
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+
+app.use('/', bodyParser.urlencoded({extended: false}));
 
 app.use(cors());
 
@@ -18,6 +24,31 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+const originalUrls = []
+const shortUrls = []
+
+app.post('/api/shorturl', (req, res) => {
+  const url = req.body.url
+  const foundIndex = originalUrls.indexOf(url)
+
+  if(foundIndex < 0){
+    originalUrls.push(url)
+    shortUrls.push(shortUrls.length)
+
+    return res.json({
+      original_url: url,
+      short_url: shortUrls.length -1
+    })
+  }
+
+  return res.json({
+    original_url: url,
+    short_url: shortUrls[foundIndex]
+  })
+
+  res.json(req.body.url);
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
